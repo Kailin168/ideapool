6c252429b060ff46d2b8b4cb34bfaa820ac25807
Kai Lin <103536761+Kailin168@users.noreply.github.com>
2023-02-27 13:34:43 -0500

    setting up cookies

--- ideapool-app/src/components/IdeaContainer.js
+++ ideapool-app/src/components/IdeaContainer.js
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from "react";
 import CreateNewIdeaForm from "./CreateNewIdeaForm";
 import ListOfIdeas from "./ListOfIdeas";
 import SearchBar from "./SearchBar";
+import Cookies from 'js-cookie';
+
 
 function IdeaContainer() {
 
   const [ideas, setIdeas] = useState([]);
- 
+
 
   const addNewIdea = (idea) => {
     setIdeas([...ideas, idea]);
@@ -16,14 +18,21 @@ function IdeaContainer() {
   useEffect(() => {
     fetch('http://localhost:4000/posts')
       .then(response => response.json())
-      .then(data => { setIdeas(data) });
+      .then(data => {
+        setIdeas(data)
+        Cookies.set('myCookie', 'cookieValue', { expires: 7 });
+        // Retrieve the cookie value when the component mounts
+        const myCookie = Cookies.get('myCookie');
+        console.log(myCookie);
+      });
+
   }, []);
 
   return (
     <div>
       <CreateNewIdeaForm addNewIdea={addNewIdea} />
-      <SearchBar ideas={ideas}/>
-      { ideas.length !== 0 && <ListOfIdeas ideas={ideas} setIdeas={setIdeas}/> }
+      <SearchBar ideas={ideas} />
+      {ideas.length !== 0 && <ListOfIdeas ideas={ideas} setIdeas={setIdeas} />}
     </div>
   );
 }
